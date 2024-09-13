c12a924c639bf014c80c0d03c68e35fd5aa8c139
Marcelo Rossberg <marcelorp89@gmail.com>
2024-09-13 09:46:21 -0300

    Implementamos o hello world multi linguas

--- hello.py
+++ hello.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
+"""Hello World Multi Linguas.
 
-# Comentário
-print("Hello, World!")
+Dependendo a da língua configurada no ambiente
+o programa exibe a mensagem correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+    
+    export LANG=pt_BR
+
+Execução:
+    
+    python3 hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Marcelo Rossberg Palmieri"
+__license__ = "Unlincense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde"
+
+print(msg)
